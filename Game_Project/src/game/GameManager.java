177c27f40ebdc5065e69249de9ca9db05a35fddf
Carlos-0620 <carlosdavidu8@gmail.com>
2025-03-26 11:10:14 +0000

    Implement maze generation and printing functionality in MazeGenerator class

--- Game_Project/src/game/GameManager.java
+++ Game_Project/src/game/GameManager.java
@@ -1,5 +1,14 @@
 package game;
 
+import maze.MazeGenerator;
+
 public class GameManager {
+
+    public static void main(String[] args) {
+
+     MazeGenerator maze = new MazeGenerator(20, 20);
+        maze.generateMaze();
+        maze.printMaze();
+    }
 
 }
