97ebcfd0da0eb229abde44b35dcd9f35fe18ece1
Diana Silva <diana.silva@student.griffith.ie>
2025-04-02 11:11:20 +0100

    Created a JFrame with a logo and default name.

--- Game_Project/src/graphicalUserInterface/GameWindow.java
+++ Game_Project/src/graphicalUserInterface/GameWindow.java
@@ -1,5 +1,21 @@
 package graphicalUserInterface;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
 public class GameWindow {
 
+	public static void main(String[] args) {
+		
+	JFrame frame = new JFrame(); //Creates a frame.
+	frame.setTitle("GameName"); //Sets name of the game on the frame.
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exits out of the application.
+	frame.setResizable(false); //Prevents frame form being resized.
+	frame.setSize(1000,700); //Sets the x and y dimensions of the frame.
+	frame.setVisible(true); //Makes frame visible.
+	
+	ImageIcon logo = new ImageIcon("logo.png"); //Creates an ImageIcon.
+	frame.setIconImage(logo.getImage()); //Changes icon of frame.
+	}
 }
