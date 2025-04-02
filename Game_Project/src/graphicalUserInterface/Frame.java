247a429dc29e6f2767d6c47aa33e5928fa608bea
Diana Silva <diana.silva@student.griffith.ie>
2025-04-02 12:14:57 +0100

    Class now extends GameWindow and have started to implement the game into gui.

--- Game_Project/src/graphicalUserInterface/Frame.java
+++ Game_Project/src/graphicalUserInterface/Frame.java
@@ -7,19 +7,24 @@ import javax.swing.JFrame;
 
 //Child class for GameWindow.java
 
-public class Frame extends JFrame{
+public class Frame extends GameWindow{
 
-	Frame(){
+	public Frame(MazeGame game){
+		super(game); //Calls for parents constructor with game reference,
 
-		this.setTitle("GameName"); //Sets name of the game on the this.
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exits out of the application.
-		this.setResizable(false); //Prevents this form being resized.
-		this.setSize(1000,700); //Sets the x and y dimensions of the this.
-		this.setVisible(true); //Makes this visible.
-
+		//Customisation of window settings
+		setTitle("GameName"); //Sets name of the game on the 
 		ImageIcon logo = new ImageIcon("logo.png"); //Creates an ImageIcon.
-		this.setIconImage(logo.getImage()); //Changes icon of this.
-		this.getContentPane().setBackground(new Color(64, 64, 64)); //Changes colour of the background. 
+		setIconImage(logo.getImage()); //Changes icon of frame.
+		getContentPane().setBackground(new Color(64, 64, 64)); //Changes colour of the background. 
 
+		intializeGameUI();
+	}
+	
+	private void initializeGameUI() {
+		
+	}
+	private void setBackgroundColor(Color color) {
+		getContentPane().setBackground(color);
 	}
 }
