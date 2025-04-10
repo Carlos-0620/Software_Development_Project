691323ce63ee2d84f98ffaa5b3356939b7245ea8
Carlos-0620 <carlosdavidu8@gmail.com>
2025-04-10 15:45:38 +0100

    Refactor Sprite class to implement random movement logic from main branch.

--- Game_Project/src/sprites/Sprite.java
+++ Game_Project/src/sprites/Sprite.java
@@ -1,8 +1,11 @@
 package sprites;
+import java.util.Random;
+import maze.Maze;
 
 public class Sprite {
     private int row;
     private int col;
+    private final Random random = new Random();
 
     public Sprite(int row, int col) {
         this.row = row;
@@ -17,9 +20,19 @@ public class Sprite {
         return col;
     }
 
-    public void move(int dRow, int dCol) {
-        this.row += dRow;
-        this.col += dCol;
+    public void moveRandomly(Maze maze) {
+        String[] directions = {"W", "S", "A", "D"};
+        for (int i = 0; i < 4; i++) {
+            String direction = directions[random.nextInt(directions.length)];
+            if (maze.canMoveSprite(this, direction)) {
+                switch (direction) {
+                    case "W" -> row--;
+                    case "S" -> row++;
+                    case "A" -> col--;
+                    case "D" -> col++;
+                }
+                break;
+            }
+        }
     }
 }
-
