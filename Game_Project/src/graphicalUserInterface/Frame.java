78ce5132b45c9d4cdb792fab0ac344534ba1e155
Muslim Muradov <muslim.muradov@student.griffith.ie>
2025-04-16 00:01:33 +0100

    Added MazePanel to Frame for rendering the maze and handling user inputs

--- Game_Project/src/graphicalUserInterface/Frame.java
+++ Game_Project/src/graphicalUserInterface/Frame.java
@@ -3,6 +3,7 @@ package graphicalUserInterface;
 import game.MazeGame;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 //Child class for GameWindow.java
 
@@ -12,12 +13,20 @@ public class Frame extends GameWindow{
 		super(game); //Calls for parents constructor with game reference,
 
 		//Customisation of window settings
-		setTitle("GameName"); //Sets name of the game on the 
+		setTitle("GameName"); //Sets name of the game on the
 		ImageIcon logo = new ImageIcon("logo.png"); //Creates an ImageIcon.
 		setIconImage(logo.getImage()); //Changes icon of frame.
-		getContentPane().setBackground(new Color(64, 64, 64)); //Changes colour of the background. 
+		getContentPane().setBackground(new Color(64, 64, 64)); //Changes colour of the background.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(true);
+		setSize(1080, 720);
+		setLocationRelativeTo(null);
+		setVisible(true);
 
 		initializeGameUI();
+
+		MazePanel mazePanel = new MazePanel(game);
+		add(mazePanel);
 	}
 	
 	private void initializeGameUI() {
