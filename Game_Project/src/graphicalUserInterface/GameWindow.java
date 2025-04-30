f1d2b527b8d766ee1178343deae720c87ff87314
TgTstion <goshataisaev@gmail.com>
2025-04-30 05:01:21 +0300

    encapsulate window properties

--- Game_Project/src/graphicalUserInterface/GameWindow.java
+++ Game_Project/src/graphicalUserInterface/GameWindow.java
@@ -12,19 +12,20 @@ public class GameWindow extends JFrame {
 	protected MazeGame game; //Imported game logic.
 	
 	public GameWindow(MazeGame game) {
-        this.game = game;
+		this.game = game;
+		
+    //Window Configuration.
+	setTitle("GameName"); //Sets name of the game on the frame.
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exits out of the application.
+	setResizable(false); //Prevents frame form being resized.
+	setSize(1000,700); //Sets the x and y dimensions of the 
+	setVisible(true); //Makes frame visible.
+	setLocationRelativeTo(null); //Center the window.
+	
+	//Window appearance.
+	ImageIcon logo = new ImageIcon("logo.png"); //Creates an ImageIcon.
+	setIconImage(logo.getImage()); //Changes icon of frame.
+	getContentPane().setBackground(new Color(64, 64, 64)); //Changes colour of the background. 
 
-        // Конфигурация окна
-        setTitle("GameName"); // Имя игры в заголовке окна
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Закрытие приложения при выходе
-        setResizable(false); // Отключение изменения размера окна
-        setSize(1000, 700); // Установка размеров окна
-        setVisible(true); // Отображение окна
-        setLocationRelativeTo(null); // Центровка окна на экране
-
-        // Настройка внешнего вида окна
-        ImageIcon logo = new ImageIcon("logo.png"); // Загрузка иконки
-        setIconImage(logo.getImage()); // Установка иконки окна
-        getContentPane().setBackground(new Color(64, 64, 64)); // Установка фона
-    }
+	}
 }
